0fe841dbf1705525d3a603f366133332d90e67d4
kraaass <alina.krasnobaeva90@gmail.com>
2024-08-20 14:22:33 +0200

    Meldungen

---
+++ DRIVE Simulation/Assets/Scenes/OffRoadDetection.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OffRoadDetection : MonoBehaviour
+{
+    public string roadTag = "RoadMesh"; 
+    private Dictionary<string, bool> wheelStatus; 
+    public Text statusText; 
+
+    void Start()
+    {
+        wheelStatus = new Dictionary<string, bool>
+        {
+            { "WheelHL", true },
+            { "WheelHR", true },
+            { "WheelVL", true },
+            { "WheelVR", true }
+        };
+        statusText.gameObject.SetActive(false); 
+        UpdateOffRoadStatus(); 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter aufgerufen mit Tag: " + collision.collider.tag);
+        if (wheelStatus.ContainsKey(collision.collider.tag) || collision.collider.GetComponent<MeshCollider>() != null)
+        {
+            wheelStatus[collision.collider.tag] = true; 
+            UpdateOffRoadStatus();
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("OnCollisionExit aufgerufen mit Tag: " + collision.collider.tag);
+        if (wheelStatus.ContainsKey(collision.collider.tag) || collision.collider.GetComponent<MeshCollider>() != null)
+        {
+            wheelStatus[collision.collider.tag] = false; 
+            UpdateOffRoadStatus();
+        }
+    }
+
+    void UpdateOffRoadStatus()
+    {
+        bool allOnRoad = true;
+        bool allOffRoad = true;
+        foreach (var status in wheelStatus.Values)
+        {
+            if (!status)
+            {
+                allOnRoad = false;
+            }
+            if (status)
+            {
+                allOffRoad = false;
+            }
+        }
+
+        if (allOnRoad)
+        {
+            SetOffRoadStatus(false, null);
+        }
+        else if (allOffRoad)
+        {
+            SetOffRoadStatus(true, null);
+        }
+        else
+        {
+            foreach (var wheel in wheelStatus)
+            {
+                if (!wheel.Value)
+                {
+                    SetOffRoadStatus(true, wheel.Key);
+                    return;
+                }
+            }
+        }
+    }
+
+    void SetOffRoadStatus(bool offRoad, string wheelTag)
+    {
+        Debug.Log("SetOffRoadStatus aufgerufen: " + offRoad);
+        statusText.gameObject.SetActive(true);
+        if (offRoad)
+        {
+            if (wheelTag == null)
+            {
+                statusText.text = "Das Auto ist nicht auf der Straﬂe.";
+                statusText.color = Color.red;
+                Debug.Log("Das Auto ist nicht auf der Straﬂe.");
+            }
+            else
+            {
+                statusText.text = $"Das Rad {wheelTag} hat die Straﬂe verlassen!";
+                statusText.color = Color.red;
+                Debug.Log($"Das Rad {wheelTag} hat die Straﬂe verlassen!");
+            }
+        }
+        else
+        {
+            statusText.text = "Das Auto ist auf der Straﬂe.";
+            statusText.color = new Color(0.0f, 0.5f, 0.0f);
+            Debug.Log("Das Auto ist auf der Straﬂe.");
+        }
+    }
+}
